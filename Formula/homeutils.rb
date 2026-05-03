5cf690ce9d6d65007011ef6eb260a04672d3d5e8
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-05-03 16:15:25 +1200

    Add url
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/homeutils.rb
+++ Formula/homeutils.rb
@@ -1,6 +1,7 @@
 class Homeutils < Formula
   desc "Core Utils but better! (Drop-in replacements for standard Unix commands)"
   homepage "https://github.com/norwd/homebrew-tap"
+  url "https://github.com/norwd/homebrew-tap/archive/main.tar.gz"
   license "Hippocratic-2.1+" # Strictly speaking, this should be "Hippocratic License HL3-CL-ECO-LAW-MIL-SV"
 
   depends_on "bat" # cat(1)
