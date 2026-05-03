e7aef4186bcb8c24a933d7f2c954e4b3e95fd61d
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-05-04 00:38:45 +1200

    Fix shasum
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/homeutils.rb
+++ Formula/homeutils.rb
@@ -2,7 +2,7 @@ class Homeutils < Formula
   desc "Core Utils but better! (Drop-in replacements for standard Unix commands)"
   homepage "https://github.com/norwd/homebrew-tap"
   url "https://github.com/norwd/homebrew-tap/archive/refs/tags/0.0.0.tar.gz"
-  sha256 "617dbbb7cfedc917787034f5606e2fc6c6fe5c359cb0759a178e8093543fa017"
+  sha256 "ac26105860b64c67c88de2297127494fd214743ef52f8fdc8bb0bdbc6c102340"
   license "Hippocratic-2.1+" # Strictly speaking, this should be "Hippocratic License HL3-CL-ECO-LAW-MIL-SV"
 
   depends_on "bat" # cat(1)
