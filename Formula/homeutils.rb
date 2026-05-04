4597551bd94c07342f710dce325d40cda4b14db7
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-05-04 13:35:44 +1200

    Add bfs as find replacement
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/homeutils.rb
+++ Formula/homeutils.rb
@@ -6,6 +6,7 @@ class Homeutils < Formula
   license "Hippocratic-2.1+" # Strictly speaking, this should be "Hippocratic License HL3-CL-ECO-LAW-MIL-SV"
 
   depends_on "bat" # cat(1)
+  depends_on "bfs" # find(1)
   depends_on "btop" # top(1) / htop(1)
   depends_on "ugrep" # grep(1)
 
