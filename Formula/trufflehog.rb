49af6953d76ab030fe737b0c1c2386744000c42f
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-05-12 08:55:23 +1200

    Fix whitespace
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/trufflehog.rb
+++ Formula/trufflehog.rb
@@ -39,8 +39,7 @@ class Trufflehog < Formula
 
     assert_match version.to_s, shell_output("#{bin}/trufflehog --version 2>&1")
 
-    bash_completion_output =
-    shell_output("bash -c 'source #{bash_completion}/trufflehog && complete -p trufflehog'")
+    bash_completion_output = shell_output("bash -c 'source #{bash_completion}/trufflehog && complete -p trufflehog'")
     assert_match "complete -F _trufflehog trufflehog", bash_completion_output
 
     assert_match(/^_trufflehog \(\) \{/,
