1fa46369d0a4f17e3f45a57e10748450a6f5862f
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-08-16 21:50:43 +1200

    Update formula with optimistic Vim -> EVi replacement
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/evi.rb
+++ Formula/evi.rb
@@ -1,37 +1,15 @@
-class Vim < Formula
+class EVi < Formula
   desc "Vi 'workalike' with many additional features"
-  homepage "https://www.vim.org/"
-  # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/refs/tags/v9.2.0950.tar.gz"
-  sha256 "5b8d49200fff3f486f28ae999f3ce7c269308ca4663c61f7b4efe61f918689b2"
+  homepage "https://evi-editor.codeberg.page"
+  url "https://codeberg.org/evi-editor/evi/archive/a881f66bfa3a151a28eb546b1392c2cbf62d3e92.tar.gz"
+  sha256 "a881f66bfa3a151a28eb546b1392c2cbf62d3e92"
   license "Vim"
   compatibility_version 1
-  head "https://github.com/vim/vim.git", branch: "master"
+  head "ssh://git@codeberg.org/evi-editor/evi.git", branch: "master"
 
-  # The Vim repository contains thousands of tags and the `Git` strategy isn't
-  # ideal in this context. This is an exceptional situation, so this checks the
-  # first 50 tags using the GitHub API (to minimize data transfer).
   livecheck do
-    url "https://api.github.com/repos/vim/vim/tags?per_page=50"
+    url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
-    strategy :json do |json, regex|
-      json.map do |tag|
-        match = tag["name"]&.match(regex)
-        next if match.blank?
-
-        match[1]
-      end
-    end
-    throttle 50
-  end
-
-  bottle do
-    sha256 arm64_tahoe:   "c4b8a3aa5dc7644136692ad239424052bb747137977a9b5d136b9a4241bfc31d"
-    sha256 arm64_sequoia: "0a401d537c4d5fcc66ecd62f303a01713ddfc150374f32effb91780b4e086d99"
-    sha256 arm64_sonoma:  "eee49037ff4c7be7e63203ecf67badf781326c5997e954a3265ef67a1a2d3abc"
-    sha256 sonoma:        "354ffb665de2bc1d9f36429535d93fdb3907b182d152b300ae4c432358b9a99b"
-    sha256 arm64_linux:   "e051f780bbf18c528cfa718bc8fc2dd9d14ae61207179d9a6bba5192a6e5b018"
-    sha256 x86_64_linux:  "940652a3a7f461c6a505f77ed326b584acb0e4812f86083601c60c4157903e5b"
   end
 
   depends_on "gettext" => :build
@@ -51,9 +29,9 @@ class Vim < Formula
     depends_on "acl"
   end
 
-  conflicts_with "ex-vi", because: "vim and ex-vi both install bin/ex and bin/view"
-  conflicts_with "macvim", because: "vim and macvim both install vi* binaries"
-  conflicts_with "vim-classic", because: "vim and vim-classic both install vi* binaries"
+  conflicts_with "ex-vi", because: "EVi and ex-vi both install ex, vi, and view binaries"
+  conflicts_with "macvim", because: "EVi and macvim both install ex, vi, and view binaries"
+  conflicts_with "vim-classic", because: "EVi and vim-classic both install ex, vi, and view binaries"
 
   def extra_deps = deps.select { |dep| dep.build? && dep.test? }
 
@@ -87,10 +65,10 @@ class Vim < Formula
                           "--with-lua-prefix=#{formula_opt_prefix("lua")}"
     system "make"
     # Parallel install could miss some symlinks
-    # https://github.com/vim/vim/issues/1031
+    # https://github.com/vim/vim/issues/1031 (predates forking, still applies to EVi)
     ENV.deparallelize
     system "make", "install", "prefix=#{prefix}"
-    bin.install_symlink "vim" => "vi"
+    bin.install_symlink "evi" => "vi"
   end
 
   def caveats
@@ -98,16 +76,16 @@ class Vim < Formula
   end
 
   test do
-    (testpath/"commands.vim").write <<~VIM
-      :python3 import vim; vim.current.buffer[0] = 'hello python3'
-      :ruby Vim::Buffer.current.append(0, 'hello ruby')
+    (testpath/"commands.evi").write <<~EVI
+      :python3 import evi; evi.current.buffer[0] = 'hello python3'
+      :ruby EVi::Buffer.current.append(0, 'hello ruby')
       :perl $curbuf->Append(0, "hello perl")
-      :lua vim.buffer():insert("hello lua")
+      :lua EVi.buffer():insert("hello lua")
       :wq
-    VIM
-    system bin/"vim", "-T", "dumb", "-s", "commands.vim", "test.txt"
+    EVI
+    system bin/"evi", "-T", "dumb", "-s", "commands.evi", "test.txt"
     assert_equal "hello perl\nhello ruby\nhello python3\nhello lua", File.read("test.txt").chomp
-    assert_match "+gettext", shell_output("#{bin}/vim --version")
-    assert_match "+sodium", shell_output("#{bin}/vim --version")
+    assert_match "+gettext", shell_output("#{bin}/evi --version")
+    assert_match "+sodium", shell_output("#{bin}/evi --version")
   end
 end
