814c202ce778a2217ecc73b4eba8001cd5105cd5
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-09-22 14:57:32 +1200

    Add language integration tests back
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/evi.rb
+++ Formula/evi.rb
@@ -78,15 +78,15 @@ class Evi < Formula
   end
 
   test do
-    # (testpath/"commands.evi").write <<~EVI
-    #   :python3 import evi; evi.current.buffer[0] = 'hello python3'
-    #   :ruby EVi::Buffer.current.append(0, 'hello ruby')
-    #   :perl $curbuf->Append(0, "hello perl")
-    #   :lua EVi.buffer():insert("hello lua")
-    #   :wq
-    # EVI
-    # system bin/"evi", "-T", "dumb", "-s", "commands.evi", "test.txt"
-    # assert_equal "hello perl\nhello ruby\nhello python3\nhello lua", File.read("test.txt").chomp
+    (testpath/"commands.evi").write <<~EVI
+      :python3 import evi; evi.current.buffer[0] = 'hello python3'
+      :ruby EVi::Buffer.current.append(0, 'hello ruby')
+      :perl $curbuf->Append(0, "hello perl")
+      :lua EVi.buffer():insert("hello lua")
+      :wq
+    EVI
+    system bin/"evi", "-T", "dumb", "-s", "commands.evi", "test.txt"
+    assert_equal "hello perl\nhello ruby\nhello python3\nhello lua", File.read("test.txt").chomp
     assert_match "+gettext", shell_output("#{bin}/evi --version")
     assert_match "+sodium", shell_output("#{bin}/evi --version")
   end
