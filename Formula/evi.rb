a1667ea8c6940309da4cadb50c8ca25350dca48c
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-09-23 20:54:20 +1200

    Make source just git directly
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/evi.rb
+++ Formula/evi.rb
@@ -1,7 +1,9 @@
 class Evi < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://evi-editor.codeberg.page"
-  url "https://codeberg.org/norwd-forks/evi/archive/v10.0.0.tar.gz"
+  url "https://codeberg.org/norwd-forks/evi.git"
+      tag:      "v10.0.0",
+      revision: "36a260062afe319404598121187f5bb94c3f7e08"
   sha256 "a5d4c10b29d1f933d8d2f889fca25334372a231be8612add872ecc8ac017b578"
   license "GPL-3.0-or-later"
   version_scheme 1
