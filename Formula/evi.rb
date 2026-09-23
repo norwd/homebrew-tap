b572e4e9a9d29ef91eec444c012eea73773e7a84
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-09-23 12:47:40 +1200

    Always build from HEAD
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/evi.rb
+++ Formula/evi.rb
@@ -1,8 +1,8 @@
 class Evi < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://evi-editor.codeberg.page"
-  url "https://codeberg.org/norwd-forks/evi/archive/v10.0.0.tar.gz"
-  sha256 "a5d4c10b29d1f933d8d2f889fca25334372a231be8612add872ecc8ac017b578"
+  # url "https://codeberg.org/norwd-forks/evi/archive/v10.0.0.tar.gz"
+  # sha256 "a5d4c10b29d1f933d8d2f889fca25334372a231be8612add872ecc8ac017b578"
   license "GPL-3.0-or-later"
   version_scheme 1
   compatibility_version 1
