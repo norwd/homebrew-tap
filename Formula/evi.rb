ae129136ffcca76700da7519dc659e1668e6bff9
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-09-23 12:24:49 +1200

    Embed commit sha of EVi in `--version`
    Depends-on: https://codeberg.org/evi-editor/evi/pulls/204
    
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/evi.rb
+++ Formula/evi.rb
@@ -42,6 +42,9 @@ class Evi < Formula
       ENV.append "LDFLAGS", "-Wl,-rpath,#{extra_rpath}"
     end
 
+    # Allow EVi commit sha to be shown with --version
+    ENV.append "CFLAGS", "-DEVI_VERSION_GIT_SHA='\"#{shell_output("git rev-parse --short HEAD").strip}\"'"
+
     # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
     # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
     # system vimscript files. We specify the normal installation prefix
