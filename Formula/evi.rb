9d4a5631b73976a4a03a1f314f142e5e76b3abd6
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-09-23 23:25:25 +1200

    Update evi.rb
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- Formula/evi.rb
+++ Formula/evi.rb
@@ -14,7 +14,7 @@ class Evi < Formula
     url "https://codeberg.org/api/v1/repos/norwd-forks/evi/branches/master"
     regex(/^[a-fA-F0-9]{40}$/i)
     strategy :json do |json|
-      json["commit"]["id"]&.[](regex, 1)
+      json["commit"]["id"](regex, 1)
     end
   end
 
